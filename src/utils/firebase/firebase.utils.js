78daa0519c2a48ce385b736eee61a1928325fd56
CORP\81258527 <NellyRose.T.Aporado @pccw.com>
2022-05-23 17:12:50 +0800

    Created collections for users

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,55 @@
+import { async } from '@firebase/util'
+import { initializeApp } from 'firebase/app'
+import {
+  getAuth,
+  signInWithPopup,
+  signInWithRedirect,
+  GoogleAuthProvider,
+} from 'firebase/auth'
+
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore' //getFirestore is to instantiate firestore instance just like getAuth in auth service. doc is to retreive documents inside firestore db. getDoc is to access data in doc, and setDoc when you set data in the docs.
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDnNPvw27WNTial-69t259CjaTrSAqmv3c',
+  authDomain: 'nelly-a-clothing.firebaseapp.com',
+  projectId: 'nelly-a-clothing',
+  storageBucket: 'nelly-a-clothing.appspot.com',
+  messagingSenderId: '80466810771',
+  appId: '1:80466810771:web:e2b5311fada7301520ba66',
+}
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+
+const provider = new GoogleAuthProvider()
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopUp = () => signInWithPopup(auth, provider)
+
+//instantiate firestore db
+export const db = getFirestore()
+
+export const createUserDocFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid) //doc takes 3 params: fireStore db, collections or tables, and the thing you want to return from the response. In this example it's userAuth.uid
+  const userSnapshot = await getDoc(userDocRef)
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      })
+    } catch (err) {
+      console.log('There is an error.', err.message)
+    }
+  }
+}
